19ca83013620e96e9373e6d0f37ddb45b2cbe0a2
mrkhoshghamat <mr.khoshghamat@gmail.com>
2024-03-31 01:16:07 +0330

    interface models repositories added

--- HealthTourist.Application/Contracts/Account/IPersonRepository.cs
+++ HealthTourist.Application/Contracts/Account/IPersonRepository.cs
@@ -1,6 +1,9 @@
+using HealthTourist.Application.Contracts.Persistence.Base;
+using HealthTourist.Domain.Account;
+
 namespace HealthTourist.Application.Contracts.Account;
 
-public interface IPersonRepository
+public interface IPersonRepository : IRepository<Person, long>
 {
     
 }
